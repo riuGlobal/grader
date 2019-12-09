af6826e080a7019d94390820b63ee98e54a036cb
ricardo.ortiz@trade.ec <ricardo.ortiz@trade.ec>
2019-12-08 19:39:31 -0500

    done with grade endpoint

--- src/http/processes/grade-process.js
+++ src/http/processes/grade-process.js
@@ -1,16 +1,16 @@
+import letterGrade from 'letter-grade'
+
 export const grade = async gradeData  => {
     let max = gradeData.max;
     let done = gradeData.done;
 
-    let percentage = Math.round(done/max);
-    let gradeLetter = 'a'
-    // console.log(gradeData.max)
-    // console.log(gradeData.params)
-    // let data = {}
-    // data.message = "graded"
+    let percentage = parseFloat((done*100/max).toFixed(2));
+    let gradeLetter = letterGrade(percentage)
 
     let data = {};
     data.percentage = percentage;
     data.gradeLetter = gradeLetter
+
     return data
 } 
+
